8926099d6df92885c1777ea79430c2816e4d18b4
Alina <alinabrooo@yandex.ru>
2020-05-19 18:16:18 +0300

    вторая часть практического задания, добавили цикл и условие

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,13 +8,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = promrt('На сколько его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = promrt('На сколько его оценита?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = promrt('На сколько его оцените?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = promrt('На сколько его оценита?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d; 
+// далее мы автомотизировали этот шаг с помощью цикла
  
+for (let i=0; i<2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько его оцените?', '');
 
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман!");
+} else {
+    alert("Ошибочка вышла(");
+}
+
+console.log(personalMovieDB);
